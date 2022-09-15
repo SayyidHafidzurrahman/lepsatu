b1f3c922ef2d98dcdeacdd0db3c594f62e8f3483
Sayyid Hafidzurrahman <sayyid.hafidzurrahman@ui.ac.id>
2022-09-15 12:54:21 +0700

    success

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from wishlist.views import show_wishlist
-
+from wishlist.views import parameter
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', parameter, name=’parameter’),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
@@ -7,4 +9,7 @@ def show_wishlist(request):
         'nama': 'Sayyid Hafidzurrahman Atstsaqofi'
     }
     return render(request, "wishlist.html", context)
+def parameter(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 # Create your views here.
