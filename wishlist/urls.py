0abc6a571b86ee661a44d6119ef848239ed0f070
Sayyid Hafidzurrahman <sayyid.hafidzurrahman@ui.ac.id>
2022-09-15 13:27:33 +0700

    ''

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from wishlist.views import show_wishlist
 from wishlist.views import parameter
+from wishlist.views import parajson
+from wishlist.views import paraxmlid
+from wishlist.views import parajsonid
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
-    path('xml/', parameter, name=’parameter’),
+    path('xml/', parameter, name='parameter'),
+    path('json/', parajson, name='parajson'),
+    path('json/<int:id>', paraxmlid, name='paraxmlid'), 
+    path('json/<int:id>', parajsonid, name='parajsonid'), 
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -12,4 +12,13 @@ def show_wishlist(request):
 def parameter(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def parajson(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+def paraxmlid(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def parajsonid(request, id):
+    data = BarangWishlist.objects.filter(pk=id)  
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")  
 # Create your views here.
