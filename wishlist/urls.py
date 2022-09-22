ecb884611ae2f9b81f71a63800b20d07a402e81c
Sayyid Hafidzurrahman <sayyid.hafidzurrahman@ui.ac.id>
2022-09-22 13:36:33 +0700

    lab3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import parameter
 from wishlist.views import parajson
 from wishlist.views import paraxmlid
 from wishlist.views import parajsonid
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 app_name = 'wishlist'
 
 urlpatterns = [
@@ -11,5 +14,8 @@ urlpatterns = [
     path('xml/', parameter, name='parameter'),
     path('json/', parajson, name='parajson'),
     path('json/<int:id>', paraxmlid, name='paraxmlid'), 
-    path('json/<int:id>', parajsonid, name='parajsonid'), 
+    path('json/<int:id>', parajsonid, name='parajsonid'),
+    path('register/', register, name='register'), 
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
